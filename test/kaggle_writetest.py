371717a29ebeba122f26108b3bc17589ed3f8927
Blues_Monster <Blues_Monster@users.noreply.github.com>
2021-04-25 14:34:11 +0800

    kaggle测试文件-wav信息提取

---
+++ test/kaggle_writetest.py
@@ -0,0 +1,36 @@
+import librosa
+import csv
+import os
+
+#load music
+audio_path = '.\\test'     #文件路径
+
+#本地的测试文件，帮助在kaggle上完成文件转化
+def get_d(filepath,savepath):                            #创建信息提取函数，输入分别为包含路径的音频、保存文件的名称
+    y,sr = librosa.load(filepath)                           #输出为采样率为22050的单声道从0.0秒开始
+    k = [sr]+ y.tolist()                                    #输出格式为第一位为采样频率后面为时间序列的列表
+    
+    #print('load ok')
+    
+    with open(savepath,'w',newline='') as t:                #numline是来控制空的行数的
+        writer=csv.writer(t)                                #这一步是创建一个csv的写入器
+        writer.writerow(k)                                  #写入样本数据
+
+
+
+for root, dirs, files in os.walk(audio_path):                       #读取音频文件并提取、保存其中信息
+    for name in files:                                              #遍历文件
+        end = name.split('.')
+        if end[-1] == 'wav':                                        #判断是否为wav音频文件
+            filepath = os.path.join(root,name)                      #获取包含路径的音频名称
+            path_list = root.split('\\')
+            #print(path_list)
+            i = path_list.index('input')
+            path_list[i] ='output'                                  #
+            savepath = '\\'.join(path_list)+'\\'+name.split('.')[0]+'.csv' #创建包含路径的csv文件名
+            if not os.path.exists('\\'.join(path_list)):                        #判断保存路径下的文件夹是否存在
+                os.makedirs('\\'.join(path_list))                    #不存在就创建
+            
+            #print(filepath)
+            #print(savepath)
+            get_d(filepath,savepath)                             #提取信息
