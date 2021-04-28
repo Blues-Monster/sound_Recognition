17c58d5e8d37e926e2fe892bb17a349ea05cb216
Blues_Monster <Blues_Monster@users.noreply.github.com>
2021-04-28 15:54:33 +0800

    执行文件

--- bin/main.py
+++ bin/main.py
@@ -5,24 +5,63 @@ import librosa.display
 import matplotlib.pyplot as plt
 import sklearn
 import os
+'''
+读取csv音频文件并输出音频信息
+data[i,0] = 过零率(Zero Crossing Rate)
+data[i,1] = y_harm_mean谐波均值
+data[i,2] = y_harm_var谐波方差
+data[i,3] = y_perc_mean感知激波均值
+data[i,4] = y_perc_var感知激波方差
+data[i,5] = Tempo BMP (beats per minute)（音乐节拍）
+data[i,6] = spectral_centroids_mean光谱质心均值
+data[i,7] = spectral_centroids_var光谱质心方差
+data[i,8] = spectral_rolloff_mean光谱衰减均值
+data[i,9] = spectral_rolloff_var光谱衰减方差
+data[i,10~29] = mfccs_mean#Mel-Frequency Cepstral Coefficients(梅尔频率倒谱系数)均值
+data[i,30~49] = mfccs_var#Mel-Frequency Cepstral Coefficients(梅尔频率倒谱系数)方差
+data[i,50~61] = chromagram_mean色度频率均值
+data[i,62~73] = chromagram_var色度频率方差
+data[i,74] = chroma_stft_mean频率均值
+data[i,75] = chroma_stft_var频率方差
+'''
 
 #load music
-audio_path = '../input/gtzan-dataset-music-genre-classification/Data/genres_original'
 
+#判别程序
 
-def get_data(y,sr,name):
+audio_path = ''
+y,sr = librosa.load(audio_path)   #输出为采样率为22050的单声道从0.0秒开始
+
+def get_data(y,sr):
     audio_file, _ = librosa.effects.trim(y)
-    data = np.zeros(shape = (1,78))
+
+    librosa.display.waveplot(y, sr=sr)
+
+
+    #librosa.amplitude_to_db(S, ref=1.0)    转dB
+
+    #librosa.stft(y, n_fft=2048, hop_length=None, win_length=None, window='hann', center=True, pad_mode='reflect')      傅里叶变换
+
+    #librosa.display.specshow(data,  x_axis=None, y_axis=None, sr=22050, hop_length=512)    获得频谱
+
+    #tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    data = np.zeros(shape = (1,76))
+
     i=0
 
     #------------------------
+
     #过零率(Zero Crossing Rate)
+
     zero_crossings = librosa.zero_crossings(audio_file, pad=False)
+    #print(zero_crossings)
     data[i,0] = sum(zero_crossings)
 
     #------------------------
     #Harmonics and Perceptrual
     y_harm, y_perc = librosa.effects.hpss(audio_file)
+    #print(y_harm)#谐波
+    #print(y_perc)#感知激波
     y_harm_mean = np.mean(y_harm)
     y_harm_var = np.var(y_harm)
     y_perc_mean = np.mean(y_perc)
@@ -34,33 +73,44 @@ def get_data(y,sr,name):
     data[i,4] = y_perc_var
 
     #------------------------
+
     #Tempo BMP (beats per minute)（音乐节拍）
     tempo, _ = librosa.beat.beat_track(y, sr = sr)
+    #print(tempo)
     data[i,5] = tempo
 
     #------------------------
+
     #Spectral Centroid(光谱质心)
     # Calculate the Spectral Centroids
     spectral_centroids = librosa.feature.spectral_centroid(audio_file, sr=sr)[0]
     spectral_centroids_mean = np.mean(spectral_centroids)
     spectral_centroids_var = np.var(spectral_centroids)
     # Shape is a vector
+    #print('Centroids:', spectral_centroids, '\n')
+    #print('Shape of Spectral Centroids:', spectral_centroids.shape, '\n')
+    #print(spectral_centroids_mean)
+    #print(spectral_centroids_var)
 
     data[i,6] = spectral_centroids_mean
     data[i,7] = spectral_centroids_var
 
+
+    '''
     # Computing the time variable for visualization
     frames = range(len(spectral_centroids))
-
+    
     # Converts frame counts to time (seconds)
     t = librosa.frames_to_time(frames)
-
+    #print('frames:', frames, '\n')
+    #print('t:', t)
 
     # Function that normalizes the Sound Data
     def normalize(x, axis=0):
         return sklearn.preprocessing.minmax_scale(x, axis=axis)
-
+    '''
     #-----------------------
+
     #Spectral Rolloff(光谱衰减)
     # Spectral RollOff Vector
     spectral_rolloff = librosa.feature.spectral_rolloff(audio_file, sr=sr)[0]
@@ -77,10 +127,13 @@ def get_data(y,sr,name):
     #plt.plot(t, normalize(spectral_rolloff), color='#FFB100')
 
     #-----------------------
+
     #Mel-Frequency Cepstral Coefficients(梅尔频率倒谱系数)
     mfccs = librosa.feature.mfcc(audio_file, sr=sr)
+    #print('mfccs shape:', (np.shape(mfccs))
     mfccs_mean = np.mean(mfccs,axis=1)
     mfccs_var = np.var(mfccs,axis=1)
+    #print(np.shape(mfccs_mean))
 
     for mfccs_i in range(20):
         data[i,10+mfccs_i] = mfccs_mean[mfccs_i]
@@ -100,12 +153,15 @@ def get_data(y,sr,name):
     librosa.display.specshow(mfccs, sr=sr, x_axis='time', cmap = 'cool')
     '''
     #------------------------
+
     #Chroma Frequencies(色度频率)
-    #Increase or decrease hop_length to change how granular you want your data to be
+    # Increase or decrease hop_length to change how granular you want your data to be
     hop_length = 5000
 
     # Chromogram
     chromagram = librosa.feature.chroma_stft(audio_file, sr=sr, hop_length=hop_length)
+    #print('Chromogram shape:', chromagram.shape)
+
     chromagram_mean = np.mean(chromagram,axis=1)
     chromagram_var = np.var(chromagram,axis=1)
 
@@ -113,7 +169,11 @@ def get_data(y,sr,name):
         data[i,50+chromagram_i] = chromagram_mean[chromagram_i]
         data[i,62+chromagram_i] = chromagram_var[chromagram_i]
 
+    #plt.figure(figsize=(16, 6))
+    #librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm')
+
     #----------------------
+
     #Fourier Transform(傅里叶变换)
     # Default FFT window size
     n_fft = 2048                    # FFT window size
@@ -122,24 +182,18 @@ def get_data(y,sr,name):
     D = np.abs(librosa.stft(audio_file, n_fft = n_fft, hop_length = hop_length))    #振幅D=（f，t）
     frequent_weights  = D.sum(axis=1)               #各频率权重时间和（行为频率列为时间）
     frequent_list = librosa.fft_frequencies(sr=sr, n_fft = n_fft)/sr
-
+    #print(np.shape(frequent_list))
+    #print(np.shape(frequent_weight))
+    #print(frequent_list)
+    #print(frequent_weight)
     chroma_stft_mean = np.average(frequent_list,weights=frequent_weights)
     chroma_stft_var = sum(((frequent_list-chroma_stft_mean)*frequent_weights)**2)/sum(frequent_weights)
+    #print(chroma_stft_mean)
+    #print(chroma_stft_var)
 
     data[i,74] = chroma_stft_mean
     data[i,75] = chroma_stft_var
-    data[i,77] = name.split('.')[-2]
-    data = data.tolist()
-    data[i][76] = name.split('.')[0]
-    print(name)
+
     return data
 
 
-for root, dirs, files in os.walk(audio_path):                       #读取音频文件并提取、保存其中信息
-    for name in files:                                              #遍历文件
-        end = name.split('.')
-        if end[-1] == 'wav':                                        #判断是否为wav音频文件
-            y,sr = librosa.load(root+'/'+name)   #输出为采样率为22050的单声道从0.0秒开始
-            with open('../working/music.csv','a') as t:                #numline是来控制空的行数的
-                writer=csv.writer(t)                                #这一步是创建一个csv的写入器
-                writer.writerows(get_data(y,sr,name))                                  #写入样本数据
